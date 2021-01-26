23dd1170c82c5c411fb0b6d5222a686b5c837847
Zehra Nisar <zyphuscode@gmail.com>
2021-01-26 18:08:54 +1100

    Search for term recipes working.

--- app.js
+++ app.js
@@ -5,44 +5,48 @@ const mealsEl = document.getElementById("meals");
 const resultHeading = document.getElementById("result-heading");
 const single_mealEL = document.getElementById("single-meal");
 
-const apiKey = "a9b8493813c64a31bca4da68c77cb990";
+//const apiKey = "a9b8493813c64a31bca4da68c77cb990";
 
 
 //Search meal and fetch from API
-function searchMeal(e) {
-e.preventDefault();
+function searchMeal(e){
+ e.preventDefault();
 
-// clear singleMeal
-single_mealEL.innerHTML = " ";
+ //clear single Meal
+ single_mealEL.innerHTML = " ";
 
-//get search term
-const term = search.value;
-console.log(term);
+ //get search term
+ const term = search.value;
+ console.log(term);
 
-//check for empty
-if(term.trim()){
-  fetch(`https://api.spoonacular.com/recipes/complexSearch?s=${term}&apiKey=${apiKey}`)
-  .then(res => res.json())
-  .then(data => {
-   console.log(data);
+ //check for empty search term
+ if(term.trim()){
+   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+   .then(res => res.json())
+   .then(data => {
+     console.log(data);
+     resultHeading.innerHTML = `<h2>Search results for '${term}': </h2>`
 
-   resultHeading.innerHTML= `<h2>Search results for "${term}" :</h2>`
-
-   if(data.meals === null){
-     resultHeading.heading.innerHTML = `<p>There are no search result. Try again. </p>`;
-   } else {
-     mealsEl.innerHTML = data.meals.map(meal => `
+     if(data.meals === null){
+       resultHeading.innerHTML = `<p>There are no search result. Try again. </p>`
+     } else {
+       mealsEl.innerHTML = data.meals.map(meal => `
      <div class="meal">
-     <img src ="${meal.img}" alt="${meal.alt}"/>
-     <div class="meal-info" data-mealId="${meal.id}">
-      <h3>${meal.title}</h3>
-     </div>
-     </div>
-    `)
-    .join("");
-   }
-  })
-}
+       <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+       <div class="meal-info" data-mealId="${meal.idMeal}">
+         <h3>${meal.strMeal}</h3>
+       </div>
+      </div>
+      `)
+       .join(" ");
+       }
+   });
+
+   //clear search text
+   search.value = " ";
+ } else {
+   alert("Please enter a search value");
+ }
 }
 
 
