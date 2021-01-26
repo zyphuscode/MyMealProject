e7dc0730b70e98a4713dc9cf418ac8bdb59926d3
Zehra Nisar <zyphuscode@gmail.com>
2021-01-26 15:34:27 +1100

    Started with js functionality.

---
+++ app.js
@@ -0,0 +1,33 @@
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const random = document.getElementById("random");
+const mealsEl = document.getElementById("meals");
+const resultHeading = document.getElementById("result-heading");
+const single_mealEL = document.getElementById("single-meal");
+
+
+//Search meal and fetch from API
+function searchMeal() {f
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1 event listeners
+submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
